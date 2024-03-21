6328a819fd41ce7272a4aaba8c15fd34c684ebe1
German Gonzalo Saracca <gersaracca@gmail.com>
2024-03-20 23:55:48 -0300

    chore: husky working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const isTrue = (val: unknown): boolean => val == true
+export const isTrue = (val: unknown): boolean => val === true
 
 export const isBoolean = (val: unknown): boolean => typeof val === 'boolean'
